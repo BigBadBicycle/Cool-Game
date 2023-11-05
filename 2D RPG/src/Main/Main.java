c29a52c005f4b9b73110860932d5d3d77430a3c0
Santiago <Santiago@DESKTOP-ADR081O>
2023-11-05 17:33:16 -0600

    Initial Commit.

---
+++ 2D RPG/src/Main/Main.java
@@ -0,0 +1,24 @@
+package Main;
+
+import Inventory_System.InventoryPanel;
+import Scenes.Menu;
+import java.awt.Component;
+import java.awt.LayoutManager;
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(3);
+    Menu background = new Menu();
+    frame.add((Component)background);
+    frame.pack();
+    frame.setTitle("RPG Thing");
+    frame.setLocationRelativeTo((Component)null);
+    frame.setLayout((LayoutManager)null);
+    frame.setResizable(false);
+    frame.setVisible(true);
+    InventoryPanel invPanel = new InventoryPanel();
+    //my mom
+  }
+}
---
+++ 2D RPG/src/Mechanics/Health.java
@@ -0,0 +1,24 @@
+package Mechanics;
+
+public class Health {
+  final Integer defaultHP = Integer.valueOf(100);
+  
+  public Integer HP = this.defaultHP;
+  
+  public boolean canHealUp;
+  
+  public boolean isDead;
+  
+  public void canHeal() {
+    if (this.HP.intValue() > 0 && this.HP.intValue() < 100) {
+      this.canHealUp = true;
+      this.isDead = false;
+    } else if (this.HP.intValue() == 0) {
+      this.canHealUp = false;
+      this.isDead = true;
+    } else {
+      this.canHealUp = false;
+      this.isDead = false;
+    } 
+  }
+}
