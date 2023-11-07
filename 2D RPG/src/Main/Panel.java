2951f2d75b94a2aa426b3f4484f191cffe2f1317
Santiago <Santiago@DESKTOP-ADR081O>
2023-11-06 22:01:01 -0600

    2nd

--- 2D RPG/src/Main/Panel.java
+++ 2D RPG/src/Main/Panel.java
@@ -15,40 +15,24 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
 public class Panel extends JPanel implements Runnable, ActionListener {
+	
   final int P_WIDTH = 700;
-  
   final int P_HEIGHT = 600;
-  
   int hp;
-  
-  JButton[] panelButtons = new JButton[5];
-  
+  JButton[] panelButtons;
   JButton doAction;
-  
   JButton option1;
-  
   JButton option2;
-  
   JButton option3;
-  
   JButton option4;
-  
   JProgressBar[] bars = new JProgressBar[2];
-  
   JProgressBar health;
-  
   JProgressBar hunger;
-  
   JLabel[] labels = new JLabel[3];
-  
   JLabel healthLabel;
-  
   JLabel hungerLabel;
-  
   JLabel title;
-  
   JPanel title_panel = new JPanel();
-  
   Thread gameLoop;
   
   public Panel() {
@@ -57,35 +41,45 @@ public class Panel extends JPanel implements Runnable, ActionListener {
     
     setPreferredSize(new Dimension(700, 600));
     setBackground(Color.black);
-    this.health = new JProgressBar();
-    this.hunger = new JProgressBar();
-    this.bars[0] = this.health;
-    this.bars[1] = this.hunger;
-    this.bars[0].setValue(HClass.HP.intValue());
-    this.bars[0].setBounds(200, 540, 300, 25);
-    this.bars[0].setForeground(new Color(49, 232, 110));
-    this.bars[0].setBackground(new Color(201, 32, 6));
-    this.bars[1].setValue(100);
-    this.bars[1].setBounds(200, 570, 300, 25);
-    this.bars[1].setForeground(new Color(189, 134, 32));
-    this.bars[1].setBackground(new Color(209, 209, 13));
-    this.healthLabel = new JLabel("Health");
-    this.hungerLabel = new JLabel("Hunger");
-    this.title = new JLabel("Story");
-    this.labels[0] = this.healthLabel;
-    this.labels[1] = this.hungerLabel;
-    this.labels[2] = this.title;
-    this.doAction = new JButton("Action Button");
-    this.option1 = new JButton("Option 1");
-    this.option2 = new JButton("Option 2");
-    this.option3 = new JButton("Option 3");
-    this.option4 = new JButton("Option 4");
-    this.doAction = this.panelButtons[0];
-    this.option1 = this.panelButtons[1];
-    this.option2 = this.panelButtons[2];
-    this.option3 = this.panelButtons[3];
-    this.option4 = this.panelButtons[4];
     
+    //bars
+    health = new JProgressBar();
+    hunger = new JProgressBar();
+    bars[0] = this.health;
+    bars[1] = this.hunger;
+    bars[0].setValue(HClass.HP.intValue());
+    bars[0].setBounds(200, 540, 300, 25);
+    bars[0].setForeground(new Color(49, 232, 110));
+    bars[0].setBackground(new Color(201, 32, 6));
+    bars[1].setValue(100);
+    bars[1].setBounds(200, 570, 300, 25);
+    bars[1].setForeground(new Color(189, 134, 32));
+    bars[1].setBackground(new Color(209, 209, 13));
+    
+    //labels
+    healthLabel = new JLabel("Health");
+    hungerLabel = new JLabel("Hunger");
+    title = new JLabel("Story");
+    labels[0] = this.healthLabel;
+    labels[1] = this.hungerLabel;
+    labels[2] = this.title;
+    
+    //button
+    panelButtons = new JButton[5];
+    
+    doAction = new JButton();
+    option1 = new JButton();
+    option2 = new JButton();
+    option3 = new JButton();
+    option4 = new JButton();
+    
+    panelButtons[0] = doAction;
+    panelButtons[1] = option1;
+    panelButtons[2] = option2;
+    panelButtons[3] = option3;
+    panelButtons[4] = option4;
+    
+    //for Labels
     for (int j = 0; j < 3; j++) {
       this.labels[j].setSize(100, 100);
       this.labels[j].setForeground(Color.white);
@@ -93,19 +87,25 @@ public class Panel extends JPanel implements Runnable, ActionListener {
       this.labels[j].setFont(new Font("Roboto", 1, 15));
       add(this.labels[j]);
     } 
+   
     this.labels[0].setBounds(this.bars[0].getX() - 80, this.bars[0].getY() - 38, this.labels[2].getWidth(), this.labels[2].getHeight());
     this.labels[1].setBounds(this.bars[1].getX() - 80, this.bars[1].getY() - 38, this.labels[2].getWidth(), this.labels[2].getHeight());
     this.labels[2].setBounds(300, 300, this.labels[2].getWidth(), this.labels[2].getHeight());
+    
+    //for bars
     for (int i = 0; i < 2; i++) {
       this.bars[i].setStringPainted(true);
       add(this.bars[i]);
     } 
+    
+    //for Buttons
     for (int k = 0; k < 5; k++) {
       this.panelButtons[k].setFocusable(false);
-      this.panelButtons[k].setSize(300, 300);
+      this.panelButtons[k].setSize(50, 100);
       this.panelButtons[k].setFont(new Font("Roboto", 1, 15));
       add(this.panelButtons[k]);
     } 
+    
     setLayout((LayoutManager)null);
   }
   
